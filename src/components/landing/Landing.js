a3d53df03ee0b24fc2547df85bacbc016aad2b48
Tom <tombinnie@protonmail.com>
2022-10-05 21:06:33 +0700

    update bg image accreditation

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -7,9 +7,18 @@ const Landing = (props) => {
   return (
     <section className="bg-[url('./imgs/forest.jpg')] bg-center bg-cover h-screen flex flex-col">
       <Search />
-      <Navbar cart ={props.cart} />
+      <Navbar cart={props.cart} />
       <Splash />
-      <p className="text-emerald-300 absolute bottom-5  left-[15%] lg:left-5 text-shadow">Photo by <a href="https://unsplash.com/@martinsanchez?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Martin Sanchez</a> on <a href="https://unsplash.com/s/photos/stream?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+      <p className="text-emerald-300 absolute bottom-5  left-[15%] lg:left-5 text-shadow">
+        Photo by{" "}
+        <a href="https://unsplash.com/ja/@anellam_arts?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Malena Schmid
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/s/photos/water?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Unsplash
+        </a>
+      </p>
     </section>
   );
 };
